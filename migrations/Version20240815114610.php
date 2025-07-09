a74b97bc32ab1c6adc7da84ae1e96750965abe7c
Dmitry Balobanov <balobanov.dmitriy.78@gmail.com>
2025-07-09 09:17:01 +0300

    Fix migration order

--- migrations/Version20240815114610.php
+++ migrations/Version20240815114610.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250620131321 extends AbstractMigration
+final class Version20240815114610 extends AbstractMigration
 {
     public function getDescription(): string
     {
